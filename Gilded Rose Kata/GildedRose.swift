a93ba29720b0081cb24742f7a87d1ad52429c0e8
blakemacnair <blakemacnair@gmail.com>
2018-02-20 10:56:43 -0600

    Add base classes for the Gilded Rose and Items.

---
+++ Gilded Rose Kata/GildedRose.swift
@@ -0,0 +1,13 @@
+//
+//  GildedRose.swift
+//  Gilded Rose Kata
+//
+//  Created by Blake Macnair on 2/20/18.
+//  Copyright © 2018 Blake Macnair. All rights reserved.
+//
+
+import UIKit
+
+class GildedRose: NSObject {
+
+}
---
+++ Gilded Rose Kata/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Gilded Rose Kata
+//
+//  Created by Blake Macnair on 2/20/18.
+//  Copyright © 2018 Blake Macnair. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+
+}
