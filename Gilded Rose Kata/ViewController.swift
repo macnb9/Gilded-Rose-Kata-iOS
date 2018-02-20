95bd0996c952f1f9c87ddb74525967b841fc41ef
blakemacnair <blakemacnair@gmail.com>
2018-02-20 10:54:33 -0600

    Initial Commit

---
+++ Gilded Rose Kata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gilded Rose Kata
+//
+//  Created by Blake Macnair on 2/20/18.
+//  Copyright © 2018 Blake Macnair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
