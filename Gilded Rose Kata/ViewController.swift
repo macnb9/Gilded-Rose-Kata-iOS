225841f05a50f700ca314b2110ec1db99c53c051
blakemacnair <blakemacnair@gmail.com>
2018-02-20 11:18:06 -0600

    Add some setup logic for the Gilded Rose instance.

--- Gilded Rose Kata/ViewController.swift
+++ Gilded Rose Kata/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var gildedRose: GildedRose?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupGildedRose()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setupGildedRose() {
+        let items = [
+            Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
+            Item(name: "Aged Brie", sellIn: 2, quality: 0),
+            Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
+            Item(name: "Conjured Mana Cake", sellIn: 3, quality: 6)]
+
+        gildedRose = GildedRose(items: items)
     }
-
-
 }
 
