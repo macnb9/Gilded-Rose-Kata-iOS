4262f2c484833d90785e38c51f7d694eb85c8237
blakemacnair <blakemacnair@gmail.com>
2018-03-01 21:37:28 -0600

    Add basic cell class for Items in a table view.

---
+++ Gilded Rose Kata/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  Gilded Rose Kata
+//
+//  Created by Blake Macnair on 3/1/18.
+//  Copyright © 2018 Blake Macnair. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
