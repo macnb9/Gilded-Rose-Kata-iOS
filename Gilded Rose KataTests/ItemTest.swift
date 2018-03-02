2938fe66a633fe0f80571458bdb9d2172b6c867d
blakemacnair <blakemacnair@gmail.com>
2018-03-02 09:01:29 -0600

    Add empty test files for GildedRose and Item.

---
+++ Gilded Rose KataTests/ItemTest.swift
@@ -0,0 +1,12 @@
+//
+//  ItemTest.swift
+//  Gilded Rose KataTests
+//
+//  Created by Blake Macnair on 3/2/18.
+//  Copyright © 2018 Blake Macnair. All rights reserved.
+//
+
+import XCTest
+
+class ItemTest: XCTestCase {
+}
