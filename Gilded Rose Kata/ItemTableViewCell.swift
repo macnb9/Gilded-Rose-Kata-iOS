f2a456c606b33aa56ec6c0bf55628e01d7fd4a3e
blakemacnair <blakemacnair@gmail.com>
2018-03-01 22:16:18 -0600

    Add properties and a configure function to the ItemTableViewCell.

--- Gilded Rose Kata/ItemTableViewCell.swift
+++ Gilded Rose Kata/ItemTableViewCell.swift
@@ -9,16 +9,14 @@
 import UIKit
 
 class ItemTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var qualityLabel: UILabel!
+    @IBOutlet private weak var sellinLabel: UILabel!
+    
+    func configure(for item: Item) {
+        titleLabel.text = item.name
+        qualityLabel.text = "\(item.quality)"
+        sellinLabel.text = "\(item.sellIn)"
+    }
 }
